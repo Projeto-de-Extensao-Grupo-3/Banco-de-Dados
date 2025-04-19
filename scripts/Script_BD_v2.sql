52c694ec5a7524cadd7846791bd861695a60142a
bruno-yuji <bruno.ytakahashi@sptech.school>
2025-04-19 18:41:03 -0300

    Modelagem e script de criação de tabelas atualizado
    - Adicionei tabelas para categoria de itens do estoque;
    - Adicionei tabela para identificação do local de armazenamento;
    - Adicionei relacionamento entre funcionario e lote (permitir identificar quem registrou o lote);
    - Atualizei o script de criação do banco de dados de acordo com a modelagem.

--- scripts/Script_BD_v2.sql
+++ scripts/Script_BD_v2.sql
@@ -1,163 +1,136 @@
 DROP DATABASE IF EXISTS projeto_extensao;
-CREATE SCHEMA IF NOT EXISTS `projeto_extensao`;
-USE `projeto_extensao`;
+CREATE DATABASE projeto_extensao;
+USE projeto_extensao;
 
-CREATE TABLE IF NOT EXISTS `projeto_extensao`.`item_estoque` (
-  `id_item_estoque` INT NOT NULL AUTO_INCREMENT,
-  `categoria` VARCHAR(45) NULL,
-  `descricao` VARCHAR(45) NULL,
-  `complemento` VARCHAR(60) NULL,
-  `peso` DECIMAL(5,2) NULL,
-  `qtd_minimo` DECIMAL(5,2) NULL,
-  `qtd_armazenado` DECIMAL(5,2) NULL,
-  PRIMARY KEY (`id_item_estoque`))
-;
+CREATE TABLE IF NOT EXISTS `funcionario` (
+  `id_funcionario` INT PRIMARY KEY AUTO_INCREMENT,
+  `nome` VARCHAR(80),
+  `cpf` CHAR(11),
+  `telefone` CHAR(12),
+  `email` VARCHAR(60),
+  `senha` VARCHAR(45)
+);
 
-CREATE TABLE IF NOT EXISTS `projeto_extensao`.`servico_terceiro` (
-  `id_servico_terceiro` INT NOT NULL AUTO_INCREMENT,
-  `categoria` VARCHAR(45) NULL,
-  `nome` VARCHAR(60) NULL,
-  `telefone` CHAR(12) NULL,
-  `email` VARCHAR(45) NULL,
-  `endereco` VARCHAR(80) NULL,
-  `identificacao` VARCHAR(20) NULL,
-  PRIMARY KEY (`id_servico_terceiro`))
-;
+CREATE TABLE IF NOT EXISTS `permissao` (
+  `id_permissao` INT PRIMARY KEY AUTO_INCREMENT,
+  `descricao` VARCHAR(45) NOT NULL
+);
 
-CREATE TABLE IF NOT EXISTS `projeto_extensao`.`lote` (
-  `id_lote` INT NOT NULL AUTO_INCREMENT,
-  `descricao` VARCHAR(45) NULL,
-  `dt_entrada` DATETIME NULL,
-  `fk_servico_terceiro` INT NOT NULL,
-  PRIMARY KEY (`id_lote`),
-  INDEX `fk_lote_servico_terceiro1_idx` (`fk_servico_terceiro` ASC) VISIBLE,
-  CONSTRAINT `fk_lote_servico_terceiro1`
-    FOREIGN KEY (`fk_servico_terceiro`)
-    REFERENCES `projeto_extensao`.`servico_terceiro` (`id_servico_terceiro`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-;
-
-CREATE TABLE IF NOT EXISTS `projeto_extensao`.`lote_item_estoque` (
-  `fk_lote` INT NOT NULL,
-  `fk_item_estoque` INT NOT NULL,
-  `qtd_item` DECIMAL(5,2) NULL,
-  `preco` DECIMAL(5,2) NULL,
-  PRIMARY KEY (`fk_lote`, `fk_item_estoque`),
-  INDEX `fk_item_estoque_has_lote_lote1_idx` (`fk_item_estoque` ASC) VISIBLE,
-  INDEX `fk_item_estoque_has_lote_item_estoque1_idx` (`fk_lote` ASC) VISIBLE,
-  CONSTRAINT `fk_item_estoque_has_lote_item_estoque1`
-    FOREIGN KEY (`fk_lote`)
-    REFERENCES `projeto_extensao`.`item_estoque` (`id_item_estoque`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_item_estoque_has_lote_lote1`
-    FOREIGN KEY (`fk_item_estoque`)
-    REFERENCES `projeto_extensao`.`lote` (`id_lote`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-;
-
-CREATE TABLE IF NOT EXISTS `projeto_extensao`.`funcionario` (
-  `id_funcionario` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(80) NULL,
-  `cpf` CHAR(11) NULL,
-  `telefone` CHAR(12) NULL,
-  `email` VARCHAR(60) NULL,
-  `senha` VARCHAR(45) NULL,
-  PRIMARY KEY (`id_funcionario`))
-;
-
-CREATE TABLE IF NOT EXISTS `projeto_extensao`.`saida_estoque` (
-  `id_saida_estoque` INT NOT NULL AUTO_INCREMENT,
-  `data` DATE NULL,
-  `hora` TIME NULL,
-  `qtd_saida` DECIMAL(5,2) NULL,
-  `motivo_saida` VARCHAR(80) NULL,
-  `fk_responsavel` INT NOT NULL,
-  `fk_lote` INT NOT NULL,
-  `fk_item_estoque` INT NOT NULL,
-  `fk_costureira` INT NOT NULL,
-  PRIMARY KEY (`id_saida_estoque`),
-  INDEX `fk_log_estoque_funcionario1_idx` (`fk_responsavel` ASC) VISIBLE,
-  INDEX `fk_log_estoque_lote_item_estoque1_idx` (`fk_lote` ASC, `fk_item_estoque` ASC) VISIBLE,
-  INDEX `fk_saida_estoque_servico_terceiro1_idx` (`fk_costureira` ASC) VISIBLE,
-  CONSTRAINT `fk_log_estoque_funcionario1`
-    FOREIGN KEY (`fk_responsavel`)
-    REFERENCES `projeto_extensao`.`funcionario` (`id_funcionario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_log_estoque_lote_item_estoque1`
-    FOREIGN KEY (`fk_lote` , `fk_item_estoque`)
-    REFERENCES `projeto_extensao`.`lote_item_estoque` (`fk_lote` , `fk_item_estoque`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_saida_estoque_servico_terceiro1`
-    FOREIGN KEY (`fk_costureira`)
-    REFERENCES `projeto_extensao`.`servico_terceiro` (`id_servico_terceiro`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-;
-
-CREATE TABLE IF NOT EXISTS `projeto_extensao`.`confeccao_roupa` (
-  `fk_roupa` INT NOT NULL,
-  `fk_tecido` INT NOT NULL,
-  PRIMARY KEY (`fk_roupa`, `fk_tecido`),
-  INDEX `fk_item_estoque_has_item_estoque_item_estoque4_idx` (`fk_tecido` ASC) VISIBLE,
-  INDEX `fk_item_estoque_has_item_estoque_item_estoque3_idx` (`fk_roupa` ASC) VISIBLE,
-  CONSTRAINT `fk_item_estoque_has_item_estoque_item_estoque3`
-    FOREIGN KEY (`fk_roupa`)
-    REFERENCES `projeto_extensao`.`item_estoque` (`id_item_estoque`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_item_estoque_has_item_estoque_item_estoque4`
-    FOREIGN KEY (`fk_tecido`)
-    REFERENCES `projeto_extensao`.`item_estoque` (`id_item_estoque`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-;
-
-CREATE TABLE IF NOT EXISTS `projeto_extensao`.`permissao` (
-  `id_permissao` INT NOT NULL AUTO_INCREMENT,
-  `descricao` VARCHAR(45) NULL,
-  PRIMARY KEY (`id_permissao`))
-;
-
-CREATE TABLE IF NOT EXISTS `projeto_extensao`.`controle_acesso` (
+CREATE TABLE IF NOT EXISTS `controle_acesso` (
   `fk_funcionario` INT NOT NULL,
   `fk_permissao` INT NOT NULL,
   PRIMARY KEY (`fk_funcionario`, `fk_permissao`),
-  INDEX `fk_funcionario_has_permissao_permissao1_idx` (`fk_permissao` ASC) VISIBLE,
-  INDEX `fk_funcionario_has_permissao_funcionario1_idx` (`fk_funcionario` ASC) VISIBLE,
-  CONSTRAINT `fk_funcionario_has_permissao_funcionario1`
-    FOREIGN KEY (`fk_funcionario`)
-    REFERENCES `projeto_extensao`.`funcionario` (`id_funcionario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_funcionario_has_permissao_permissao1`
-    FOREIGN KEY (`fk_permissao`)
-    REFERENCES `projeto_extensao`.`permissao` (`id_permissao`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-;
+  FOREIGN KEY (`fk_funcionario`) REFERENCES `funcionario` (`id_funcionario`),
+  FOREIGN KEY (`fk_permissao`) REFERENCES `permissao` (`id_permissao`)
+);
 
-CREATE TABLE IF NOT EXISTS `projeto_extensao`.`corte_tecido` (
+CREATE TABLE IF NOT EXISTS `local_armazenameno` (
+  `id_local_armazenameno` INT PRIMARY KEY AUTO_INCREMENT,
+  `num_prateleira` VARCHAR(10) UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `categoria` (
+  `id_categoria` INT PRIMARY KEY AUTO_INCREMENT,
+  `nome` VARCHAR(45),
+  `fk_categoria_pai` INT,
+  FOREIGN KEY (`fk_categoria_pai`) REFERENCES `categoria` (`id_categoria`)
+);
+
+CREATE TABLE IF NOT EXISTS `item_estoque` (
+  `id_item_estoque` INT PRIMARY KEY AUTO_INCREMENT,
+  `fk_categoria` INT NOT NULL,
+  `fk_local_armazenamento` INT NOT NULL,
+  `descricao` VARCHAR(45),
+  `complemento` VARCHAR(60),
+  `peso` DECIMAL(5,2),
+  `qtd_minimo` DECIMAL(5,2),
+  `qtd_armazenado` DECIMAL(5,2),
+  FOREIGN KEY (`fk_categoria`) REFERENCES `categoria` (`id_categoria`),
+  FOREIGN KEY (`fk_local_armazenamento`) REFERENCES `local_armazenameno` (`id_local_armazenameno`)
+);
+
+CREATE TABLE IF NOT EXISTS `caracteristica_item_estoque` (
+  `fk_categoria` INT NOT NULL,
+  `fk_item_estoque` INT NOT NULL,
+  PRIMARY KEY (`fk_categoria`, `fk_item_estoque`),
+  FOREIGN KEY (`fk_categoria`) REFERENCES `categoria` (`id_categoria`),
+  FOREIGN KEY (`fk_item_estoque`) REFERENCES `item_estoque` (`id_item_estoque`)
+);
+
+CREATE TABLE IF NOT EXISTS `confeccao_roupa` (
+  `fk_roupa` INT NOT NULL,
+  `fk_tecido` INT NOT NULL,
+  PRIMARY KEY (`fk_roupa`, `fk_tecido`),
+  FOREIGN KEY (`fk_roupa`) REFERENCES `item_estoque` (`id_item_estoque`),
+  FOREIGN KEY (`fk_tecido`) REFERENCES `item_estoque` (`id_item_estoque`)
+);
+
+CREATE TABLE IF NOT EXISTS `servico_terceiro` (
+  `id_servico_terceiro` INT PRIMARY KEY AUTO_INCREMENT,
+  `categoria` VARCHAR(45),
+  `nome` VARCHAR(60),
+  `telefone` CHAR(12),
+  `email` VARCHAR(45),
+  `endereco` VARCHAR(80),
+  `identificacao` VARCHAR(20)
+);
+
+CREATE TABLE IF NOT EXISTS `lote` (
+  `id_lote` INT PRIMARY KEY AUTO_INCREMENT,
+  `descricao` VARCHAR(45),
+  `dt_entrada` DATETIME,
+  `fk_servico_terceiro` INT NOT NULL,
+  `fk_responsavel` INT NOT NULL,
+  FOREIGN KEY (`fk_servico_terceiro`) REFERENCES `servico_terceiro` (`id_servico_terceiro`),
+  FOREIGN KEY (`fk_responsavel`) REFERENCES `funcionario` (`id_funcionario`)
+);
+
+CREATE TABLE IF NOT EXISTS `lote_item_estoque` (
+  `fk_item_estoque` INT,
+  `fk_lote` INT,
+  `qtd_item` DECIMAL(5,2),
+  `preco` DECIMAL(5,2),
+  PRIMARY KEY (`fk_item_estoque`, `fk_lote`),
+  FOREIGN KEY (`fk_item_estoque`) REFERENCES `item_estoque` (`id_item_estoque`),
+  FOREIGN KEY (`fk_lote`)REFERENCES `lote` (`id_lote`)
+);
+
+CREATE TABLE IF NOT EXISTS `saida_estoque` (
+  `id_saida_estoque` INT PRIMARY KEY AUTO_INCREMENT,
+  `data` DATE,
+  `hora` TIME,
+  `qtd_saida` DECIMAL(5,2),
+  `motivo_saida` VARCHAR(80),
+  `fk_responsavel` INT NOT NULL,
+  `fk_lote` INT NOT NULL,
+  `fk_item_estoque` INT NOT NULL,
+  `fk_costureira` INT NULL,
+  FOREIGN KEY (`fk_responsavel`) REFERENCES `funcionario` (`id_funcionario`),
+  FOREIGN KEY (`fk_lote`, `fk_item_estoque`) REFERENCES `lote_item_estoque` (`fk_lote`, `fk_item_estoque`),
+  FOREIGN KEY (`fk_costureira`) REFERENCES `servico_terceiro` (`id_servico_terceiro`)
+);
+
+CREATE TABLE IF NOT EXISTS `corte_tecido` (
+  `id_corte_tecido` INT NOT NULL,
   `fk_lote` INT NOT NULL,
   `fk_tecido` INT NOT NULL,
   `fk_funcionario` INT NOT NULL,
-  `inicio` DATETIME NULL,
-  `termino` DATETIME NULL,
-  PRIMARY KEY (`fk_lote`, `fk_tecido`, `fk_funcionario`),
-  INDEX `fk_lote_item_estoque_has_funcionario_funcionario1_idx` (`fk_funcionario` ASC) VISIBLE,
-  INDEX `fk_lote_item_estoque_has_funcionario_lote_item_estoque1_idx` (`fk_lote` ASC, `fk_tecido` ASC) VISIBLE,
-  CONSTRAINT `fk_lote_item_estoque_has_funcionario_lote_item_estoque1`
-    FOREIGN KEY (`fk_lote` , `fk_tecido`)
-    REFERENCES `projeto_extensao`.`lote_item_estoque` (`fk_lote` , `fk_item_estoque`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_lote_item_estoque_has_funcionario_funcionario1`
-    FOREIGN KEY (`fk_funcionario`)
-    REFERENCES `projeto_extensao`.`funcionario` (`id_funcionario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-;
+  `inicio` DATETIME,
+  `termino` DATETIME,
+  PRIMARY KEY (`id_corte_tecido`, `fk_lote`, `fk_tecido`, `fk_funcionario`),
+  FOREIGN KEY (`fk_lote` , `fk_tecido`) REFERENCES `lote_item_estoque` (`fk_lote`, `fk_item_estoque`),
+  FOREIGN KEY (`fk_funcionario`) REFERENCES `funcionario` (`id_funcionario`)
+);
+
+
+-- Trigger para atualizar quantidade armazenada em estoque com base em entrada de lote
+CREATE TRIGGER update_stock BEFORE INSERT ON lote_item_estoque
+	FOR EACH ROW UPDATE item_estoque SET qtd_armazenado = qtd_armazenado + NEW.qtd_item
+		WHERE item_estoque.id_item_estoque = NEW.fk_item_estoque;
+-- DROP TRIGGER update_stock;
+
+-- Trigger para atualizar quantidade armazenada em estoque em caso de saÍda
+CREATE TRIGGER update_stock_saida BEFORE INSERT ON saida_estoque
+	FOR EACH ROW UPDATE item_estoque SET qtd_armazenado = qtd_armazenado - NEW.qtd_saida
+		WHERE item_estoque.id_item_estoque = NEW.fk_item_estoque;
+-- DROP TRIGGER update_stock_saida;
--- scripts/Script_teste.sql
+++ scripts/Script_teste.sql
@@ -1,50 +1,156 @@
 USE projeto_extensao;
 SHOW TABLES;
 
+-- Permissões serão fixas no banco, não será necessário cadastro ou atualização
+DESC permissao;
+INSERT INTO permissao (descricao) VALUES
+	('Visualizar dashboard'),
+	('Cadastrar funcionários'),
+	('Visualizar histórico do estoque'),
+    ('Registrar movimentação do estoque'),
+	('Visualizar dados de itens do estoque'),
+	('Cadastrar itens do estoque'),
+	('Receber alertas de falta de estoque');
+SELECT * FROM permissao;
+
+-- Cadastro de funcionários.
 DESC funcionario;
-INSERT INTO funcionario VALUES(null, 'David', '83756473896', '11985647382', 'david@gmail.com', 'Sjgi%#_J>23dH');
+INSERT INTO funcionario (nome, cpf, telefone, email, senha) VALUES
+	('Bruno', '83756473891', '11985647381', 'bruno@gmail.com', '123456@'),
+	('Fernando', '83756473892', '11985647382', 'fernando@gmail.com', '123456@'),
+	('Giorgio', '83756473893', '11985647383', 'giorgio@gmail.com', '123456@'),
+	('Guilherme', '83756473894', '11985647384', 'guilherme@gmail.com', '123456@'),
+	('João', '83756473895', '11985647385', 'joao@gmail.com', '123456@'),
+	('Leandro', '83756473896', '11985647386', 'leandro@gmail.com', '123456@');
 SELECT * FROM funcionario;
 
+-- Atualização de dados de funcionário.
+UPDATE funcionario SET nome = 'Bruno Yuji', email = 'bruno.y@gmail.com.br' WHERE id_funcionario = 1;
+SELECT * FROM funcionario;
+
+-- Cadastrar as permissões de acesso do funcionário.
+INSERT INTO controle_acesso (fk_funcionario, fk_permissao) VALUES
+	(1, 1),
+	(1, 2),
+	(1, 3);
+SELECT * FROM controle_acesso;
+
+-- Listar permissões de um funcionário
+SELECT f.nome, p.descricao FROM funcionario f 
+	JOIN controle_acesso c ON f.id_funcionario = c.fk_funcionario
+	JOIN permissao p ON p.id_permissao = c.fk_permissao;
+
+-- Cadastro de itens do estoque (peças de roupa e tecidos).
 DESC item_estoque;
--- TRUNCATE TABLE item_estoque;
-INSERT INTO item_estoque VALUES (null, 'roupa', 'vestido,azul', 'florido', 1.0, null, 0);
-INSERT INTO item_estoque VALUES (null, 'roupa', 'camisa,vermelho', 'liso', 1.0, null, 0);
-INSERT INTO item_estoque VALUES (null, 'tecido', 'marrom', 'listrado', 1.0, null, 0);
+TRUNCATE TABLE item_estoque;
+INSERT INTO item_estoque (categoria, descricao, complemento, peso, qtd_minimo, qtd_armazenado) VALUES
+	('roupa', 'vestido,azul', 'florido', 1.0, 0, 0),
+	('roupa', 'camisa,vermelho', 'liso', 1.0, 0, 0),
+	('roupa', 'bermuda,cinza', 'listras vermelhas', 1.0, 0, 0),
+	('tecido', 'vermelho', 'liso', 1.0, 0, 0),
+	('tecido', 'azul', 'florido', 1.0, 0, 0),
+	('tecido', 'cinza', 'liso', 1.0, 0, 0);
 SELECT * FROM item_estoque;
 
+-- Relacionar os tecidos que compõem uma peça de roupa.
+INSERT INTO confeccao_roupa (fk_roupa, fk_tecido) VALUES
+	(1, 5),
+	(2, 4),
+	(3, 4),
+	(3, 6);
+SELECT * FROM confeccao_roupa;
+
+-- Listar tecidos que compõem cada peça de roupa;
+SELECT roupa.id_item_estoque, roupa.descricao, roupa.complemento,
+tecido.id_item_estoque, tecido.descricao, tecido.complemento
+	FROM item_estoque AS roupa 
+	JOIN confeccao_roupa AS c ON roupa.id_item_estoque = fk_roupa
+	JOIN item_estoque AS tecido ON tecido.id_item_estoque = fk_tecido
+ORDER BY roupa.id_item_estoque;
+
+-- Listar tecidos que compõem uma peça de roupa específica;
+SELECT roupa.id_item_estoque, roupa.descricao, roupa.complemento,
+tecido.id_item_estoque, tecido.descricao, tecido.complemento
+	FROM item_estoque AS roupa 
+	JOIN confeccao_roupa AS c ON roupa.id_item_estoque = fk_roupa
+	JOIN item_estoque AS tecido ON tecido.id_item_estoque = fk_tecido
+WHERE roupa.id_item_estoque = 3;
+
+-- Categorias roupa e tecido serão fixas no banco de dados.
+INSERT INTO categoria (nome) VALUES
+	('Tecido'),
+	('Roupa');
+SELECT * FROM categoria;
+
+-- Cadastro de subcategorias para tecido e roupa.
+INSERT INTO categoria (nome, fk_categoria_pai) VALUES
+	('Nylon', 1),
+	('Poliéster', 1),
+	('Algodão', 1),
+	('Seda', 1),
+	('Lã', 1),
+	('Vestido', 2),
+	('Camiseta', 2),
+	('Calça', 2),
+	('Shorts', 2),
+	('Saia', 2);
+SELECT * FROM categoria;
+
+-- Cadastro de características de tecido e produto.
+INSERT INTO caracteristica_item_estoque (nome) VALUES
+	('Azul'),
+	('Vermelho'),
+	('Verde'),
+	('Amarelo'),
+	('Listrado'),
+	('Liso'),
+	('Florido'),
+	('Grosso'),
+	('Fino');
+SELECT * FROM caracteristica_item_estoque;
+
+-- Cadastro de um serviço terceirizado (costura e fornecedor).
 DESC servico_terceiro;
-INSERT INTO servico_terceiro VAlUES (null, 'costureira', 'Maria', '11938563748', 'maria@gmail.com', 'Rua X', null);
-INSERT INTO servico_terceiro VAlUES (null, 'fornecedor', 'Best Tecidos', '11918465729', 'best_tecidos@gmail.com', 'Rua Y', null);
+TRUNCATE TABLE servico_terceiro;
+INSERT INTO servico_terceiro (categoria, nome, telefone, email, endereco) VAlUES 
+	('costureira', 'Maria', '11938563748', 'maria@gmail.com', 'Rua X'),
+	('costureira', 'Alice', '11938563748', 'alice@gmail.com', 'Rua Y'),
+	('costureira', 'Rebeca', '11938563748', 'rebeca@gmail.com', 'Rua Z'),
+	('fornecedor', 'Best Tecidos', '11918465729', 'best_tecidos@gmail.com', 'Rua 1'),
+	('fornecedor', 'Fornecedor X', '11918465729', 'fornecedorx@gmail.com', 'Rua 2'),
+	('fornecedor', 'Fornecedor Z', '11918465729', 'fornecedorys@gmail.com', 'Rua 3');
 SELECT * FROM servico_terceiro;
 
+-- Cadastro de lote de tecido ou roupa.
 DESC lote;
-INSERT INTO lote VAlUES (null, 'lote de roupas', now(), 1);
-INSERT INTO lote VAlUES (null, 'lote de tecido', now(), 2);
+TRUNCATE TABLE lote;
+INSERT INTO lote (descricao, dt_entrada, fk_servico_terceiro) VAlUES 
+	('lote de roupas', now(), 1),
+	('lote de tecido', now(), 2);
 SELECT * FROM lote;
 
--- Trigger para atualizar quantidade armazenada em estoque com base em entrada de lote
-CREATE TRIGGER update_stock BEFORE INSERT ON lote_item_estoque
-	FOR EACH ROW UPDATE item_estoque SET qtd_armazenado = qtd_armazenado + NEW.qtd_item
-		WHERE item_estoque.id_item_estoque = NEW.fk_item_estoque;
--- DROP TRIGGER update_stock;
-
--- Trigger para atualizar quantidade armazenada em estoque em caso de saída
-CREATE TRIGGER update_stock_saida BEFORE INSERT ON saida_estoque
-	FOR EACH ROW UPDATE item_estoque SET qtd_armazenado = qtd_armazenado - NEW.qtd_saida
-		WHERE item_estoque.id_item_estoque = NEW.fk_item_estoque;
--- DROP TRIGGER update_stock_saida;
-
+-- Cadastro dos itens de um lote.
 DESC lote_item_estoque;
--- TRUNCATE TABLE lote_item_estoque;
-INSERT INTO lote_item_estoque VAlUES -- teste roupa
+TRUNCATE TABLE lote_item_estoque;
+INSERT INTO lote_item_estoque (fk_lote, fk_item_estoque, qtd_item, preco) VAlUES -- teste roupa
 	(1, 1, 5, 100.0),
-    (1, 2, 3, 100.0);
-INSERT INTO lote_item_estoque VAlUES -- teste tecido
-	(2, 3, 3.5, 80.0);
+    (1, 2, 3, 150.0),
+    (1, 3, 10, 200.0);
+INSERT INTO lote_item_estoque (fk_lote, fk_item_estoque, qtd_item, preco) VAlUES -- teste tecido
+	(2, 4, 3.5, 80.0),
+	(2, 5, 6.5, 70.0),
+	(2, 6, 12.5, 120.0);
+SELECT * FROM lote_item_estoque;
 
+-- Cadastro de saída do estoque (venda e envio para costura).
 DESC saida_estoque;
-INSERT INTO saida_estoque VAlUES (null, current_date(), current_time(), 0.5, 'envio de tecido para costura', 1, 2, 3, 1);
-SELECT * FROM saida_estoque;
+TRUNCATE TABLE saida_estoque;
+INSERT INTO saida_estoque (data, hora, qtd_saida, motivo_saida, fk_responsavel, fk_lote, fk_item_estoque, fk_costureira)
+	VAlUES (current_date(), current_time(), 2.50, 'envio de tecido para costura', 1, 2, 6, 1);
 
+-- Visualização geral.
+SELECT * FROM saida_estoque;
 SELECT * FROM lote_item_estoque;
 SELECT * FROM item_estoque;
+
+SHOW TRIGGERS; -- verificar estrutura das Triggers criadas.
